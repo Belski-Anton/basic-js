b97338610a5f52c1d1a889690c5a6b2819371af8
Anton Belski <anton.belski@gmail.com>
2023-04-17 17:15:56 +0300

    feat:add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  
+  let res = str;
+  let addSum = "";
+  if (!options.hasOwnProperty("separator")) {
+    options.separator = "+";
+  }
+  if (!options.hasOwnProperty("addition")) {
+    options.addition = "";
+  }
+  if (!options.hasOwnProperty("additionSeparator")) {
+    options.additionSeparator = "|";
+  }
+  if (!options.hasOwnProperty("additionRepeatTimes")) {
+    options.additionRepeatTimes = "1";
+  }
+  addSum = options.addition;
+  for (let i = 1; i < options.additionRepeatTimes; i++) {
+    addSum += options.additionSeparator;
+    addSum += options.addition;
+  }
+  res += addSum;
+  for (let i = 1; i < options.repeatTimes; i++) {
+    res += options.separator;
+    res += str;
+    res += addSum;
+  }
+  return res;
 }
 
 module.exports = {
