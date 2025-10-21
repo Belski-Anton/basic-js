e90d218e5cb79a5855bb40709c64ea8482bb469c
Anton Belski <anton.belski@gmail.com>
2025-10-21 18:03:51 +0300

    feat: add solve getCommonCharacterCount

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -12,11 +12,24 @@ const { NotImplementedError } = require('../lib');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 
-function getCommonCharacterCount(/* s1, s2 */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getCommonCharacterCount(s1, s2 ) {
+  let count = 0;
+  const arr1 = str1.split("");
+  const arr2 = str2.split("");
+
+  for (let i = 0; i < arr1.length; i++) {
+    const char = arr1[i];
+    const index = arr2.indexOf(char);
+
+    if (index !== -1) {
+      arr2.splice(index, 1);
+      count++;
+    }
+  }
+
+  return count;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
