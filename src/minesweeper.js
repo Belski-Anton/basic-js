48df3f53b191851b18cd7856ac0daf96a6cf43dd
Anton Belski <anton.belski@gmail.com>
2025-10-27 19:31:38 +0300

    feat: add solve  minesweeper

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,74 @@ const { NotImplementedError } = require('../lib');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  let result = [];
+
+  for (let i = 0; i < matrix.length; i += 1) {
+    result[i] = [];
+
+    for (let j = 0; j < matrix[i].length; j += 1) {
+      let count = 0;
+
+      // top
+      if (i - 1 >= 0 && matrix[i - 1][j] === true) {
+        count += 1;
+      }
+
+      // bottom
+      if (i + 1 < matrix.length && matrix[i + 1][j] === true) {
+        count += 1;
+      }
+
+      // right
+      if (j + 1 < matrix[i].length && matrix[i][j + 1] === true) {
+        count += 1;
+      }
+
+      // left
+      if (j - 1 >= 0 && matrix[i][j - 1] === true) {
+        count += 1;
+      }
+
+      // top-left 
+      if (i - 1 >= 0 && j - 1 >= 0 && matrix[i - 1][j - 1] === true) {
+        count += 1;
+      }
+
+      // top-right 
+      if (
+        i - 1 >= 0 &&
+        j + 1 < matrix[i - 1].length &&
+        matrix[i - 1][j + 1] === true
+      ) {
+        count += 1;
+      }
+
+      // bottom-left
+      if (
+        i + 1 < matrix.length &&
+        j - 1 >= 0 &&
+        matrix[i + 1][j - 1] === true
+      ) {
+        count += 1;
+      }
+
+      // bottom-right
+      if (
+        i + 1 < matrix.length &&
+        j + 1 < matrix[i + 1].length &&
+        matrix[i + 1][j + 1] === true
+      ) {
+        count += 1;
+      }
+
+      result[i][j] = count;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
